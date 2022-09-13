48be0749be1ebdc0530e2310d905ecc99ad03dae
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-09-13 21:00:55 +0100

    added orbit method

--- particle.py
+++ particle.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Particle:
     def __init__(self, x, y, speedX, speedY, life, size):
         self.xPos = x
@@ -6,8 +9,23 @@ class Particle:
         self.speedX = speedX
         self.speedY = speedY
         self.life = life
-        
+        self.angle = 0
 
     def move(self, interpolation):
-        self.xPos += self.moveSpeedX * interpolation
-        self.yPos += self.moveSpeedY * interpolation
+        self.xPos += self.speedX * interpolation
+        self.yPos += self.speedY * interpolation
+
+    def orbit(self, interpolation, originX, originY):
+        self.angle += 0.025 * interpolation
+        self.xPos = (originX + math.cos(self.angle) * 150)
+        self.yPos = (originY + math.sin(self.angle) * 150)
+
+        #https://franzeus.medium.com/math-for-circular-object-movement-in-a-game-cae101474a65
+        #B.x = A.x + cos(a) * radius;
+        #B.y = A.y + sin(a) * radius;
+
+        # X := originX + cos(angle)*radius;
+        # Y := originY + sin(angle)*radius;
+        # https://gamedev.stackexchange.com/questions/9607/moving-an-object-in-a-circular-path
+        # x=a+(r*cos θ)
+        # y=b+(r*sin θ)
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import sys, pygame
+import pygame
+import sys
 from particle import Particle
 import random
 
@@ -21,145 +22,53 @@ interpolation = 0.0
 
 game_is_running = True
 
-moveGroup = []
-arcGroup = []
-bounceGroup = []
-explodeGroup = []
-updownGroup = []
-
 # Fill background
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((0, 0, 0))
 
+black = (0, 0, 0)
+peach = (255, 175, 128)
+green = (0, 104, 55)
+white = (255, 255, 255)
+
+particles = []
+
+
+NEW_SHAPE = pygame.USEREVENT
+pygame.time.set_timer(NEW_SHAPE, 1000)
 
 def updateGame(interpolation):
 
-    for part in moveGroup:
-        part.move(interpolation)
-        part.life -= 1
-        if part.life == 0:
-            moveGroup.remove(part)
-
-    for part in arcGroup:
-        part.move(interpolation)
-        part.life -= 1
-        if part.life == 0:
-            arcGroup.remove(part)
-
-    for part in bounceGroup:
-        part.move(interpolation)
-        if part.xPos >= screenWidth or part.xPos <= 0:
-            part.moveSpeedX = part.moveSpeedX * -1
-        if part.yPos >= screenHeight or part.yPos <= 0:
-            part.moveSpeedY = part.moveSpeedY * -1
-
-    for part in explodeGroup:
-        part.move(interpolation)
-        part.life -= 1
-        if part.life == 0:
-            explodeGroup.remove(part)
-
-    for part in updownGroup:
-        part.move(5)
-        part.moveSpeedX += 0.025
-        if part.moveSpeedX >= 00:
-            part.moveSpeedX = -10
-        part.moveSpeedY += 0.025
-        if part.moveSpeedY >= 10:
-            part.moveSpeedY = -10
-        if part.xPos >= screenWidth or part.xPos <= 0:
-            part.moveSpeedX = part.moveSpeedX * -1
-        if part.yPos >= screenHeight or part.yPos <= 0:
-            part.moveSpeedY = part.moveSpeedY * -1
-            
+    for part in particles:
+        part.orbit(interpolation, pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
 
     getInput()
+
 
 def draw():
     screen.blit(background, (0, 0))
 
-    for part in moveGroup:
-        pygame.draw.rect(screen, pygame.Color(10, 255, 10), (part.xPos, part.yPos, part.size, part.size))
-
-    for part in arcGroup:
-        pygame.draw.rect(screen, pygame.Color(100, 25, 100), (part.xPos, part.yPos, part.size, part.size))
-    
-    for part in bounceGroup:
-        pygame.draw.rect(screen, pygame.Color(200, 25, 10), (part.xPos, part.yPos, part.size, part.size))
-
-    for part in explodeGroup:
-        pygame.draw.rect(screen, pygame.Color(120, 125, 200), (part.xPos, part.yPos, part.size, part.size))
-
-    for part in updownGroup:
-        pygame.draw.rect(screen, pygame.Color(245, 245, 65), (part.xPos, part.yPos, part.size, part.size))
+    for part in particles:
+        pygame.draw.rect(screen, pygame.Color(10, 255, 10),
+                         (part.xPos, part.yPos, part.size, part.size))
 
     pygame.display.flip()
 
+
 def getInput():
-    if pygame.key.get_pressed()[pygame.K_q]:
-        moveGroup.append(Particle(random.randrange(0,800),random.randrange(0,10),random.randrange(-2,5),random.randrange(5,9),random.randrange(1,200), random.randrange(2,6)))
-
-    if pygame.key.get_pressed()[pygame.K_w]:
-        arcGroup.append(Particle(random.randrange(0,800),random.randrange(0,10),2,random.randrange(5,9),random.randrange(1,200), random.randrange(2,6)))
-
-    if pygame.key.get_pressed()[pygame.K_e]:
-        bounceGroup.append(Particle(random.randrange(0,800),random.randrange(0,10),2,random.randrange(5,9),random.randrange(1,200), random.randrange(5,9)))
-
-    if pygame.key.get_pressed()[pygame.K_r]:
-        bounceGroup.append(Particle(random.randrange(0,800),random.randrange(0,10),2,random.randrange(5,9),random.randrange(1,200), random.randrange(5,19)))
-
-    if pygame.key.get_pressed()[pygame.K_a]:
-        for part in bounceGroup:
-            part.moveSpeedX = random.randrange(1,5)
-            part.moveSpeedY = random.randrange(1,5)
-
-    if pygame.mouse.get_pressed()[2]:
-        for part in bounceGroup:
-            if part.xPos > pygame.mouse.get_pos()[0]:
-                part.xPos -= 1
-            if part.xPos < pygame.mouse.get_pos()[0]:
-                part.xPos += 1
-            if part.yPos > pygame.mouse.get_pos()[1]:
-                part.yPos -= 1
-            if part.yPos < pygame.mouse.get_pos()[1]:
-                part.yPos += 1
-
-    if pygame.key.get_pressed()[pygame.K_s]:
-        for part in bounceGroup:
-            if part.xPos > pygame.mouse.get_pos()[0]:
-                part.xPos = pygame.mouse.get_pos()[0]
-            if part.xPos < pygame.mouse.get_pos()[0]:
-                part.xPos = pygame.mouse.get_pos()[0]
-            if part.yPos > pygame.mouse.get_pos()[1]:
-                part.yPos = pygame.mouse.get_pos()[1]
-            if part.yPos < pygame.mouse.get_pos()[1]:
-                part.yPos = pygame.mouse.get_pos()[1]
-
-    if pygame.key.get_pressed()[pygame.K_d]:
-        for part in bounceGroup:
-            part.xPos = random.randrange(0,screenWidth)
-            part.yPos = random.randrange(0,screenHeight)
-
     if pygame.mouse.get_pressed()[0]:
-        explodeGroup.append(Particle(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1],2,random.randrange(5,9),random.randrange(1,200), random.randrange(3,5)))
+        particles.append(Particle(200, random.randrange(100, 200), 1, 1, 1, 8))
 
-    if pygame.key.get_pressed()[pygame.K_f]:
-        explodeGroup.append(Particle(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1],random.randrange(-10,10),random.randrange(-10,10),random.randrange(10,200), random.randrange(3,5)))
-
-    if pygame.key.get_pressed()[pygame.K_g]:
-        bounceGroup.append(Particle(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1],random.randrange(-10,10),random.randrange(-10,10),random.randrange(10,200), random.randrange(3,10)))
-
-    if pygame.key.get_pressed()[pygame.K_h]:
-        updownGroup.append(Particle(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1],random.randrange(-10,10),random.randrange(-10,10),random.randrange(10,200), random.randrange(3,10)))
-
-    if pygame.key.get_pressed()[pygame.K_j]:
-        bounceGroup.append(Particle(random.randrange(0,800),random.randrange(0,10),0.25,0.023,random.randrange(1,200), 10))
 
 while game_is_running:
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        if event.type == NEW_SHAPE:  
+            particles.append(Particle(200, random.randrange(100, 200), 1, 1, 1, 8))
 
     loops = 0
 
@@ -169,5 +78,8 @@ while game_is_running:
         next_game_tick += SKIP_TICKS
         loops += 1
 
-    interpolation = float(pygame.time.get_ticks() + SKIP_TICKS - next_game_tick ) / float( SKIP_TICKS )
+    interpolation = float(pygame.time.get_ticks() +
+                          SKIP_TICKS - next_game_tick) / float(SKIP_TICKS)
     draw()
+
+
